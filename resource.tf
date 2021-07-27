a2b526cf5cbee5b983f465f1a02cf6a0fac24472
himanirajput <43176597+himanirajput@users.noreply.github.com>
2021-07-27 13:27:36 +0530

    Create resource.tf

---
+++ resource.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+ region = "us-east-2"
+}
+
+resource "aws_instance" "s1" {
+ ami = "ami-0603cbe34fd08cb81"
+ instance_type = "t2.micro"
+  tags = {
+    Name = "Himani-instance"
+    Env = "Prod"
+  }
+}
